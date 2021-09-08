2b49366515db8d8345fe24e3a05efa6e50048cfd
TheVy <ntvy2k@gmail.com>
2021-09-08 07:56:52 +0700

    add password mt

--- frontend/frontend/src/features/auth/Login_FaceID.tsx
+++ frontend/frontend/src/features/auth/Login_FaceID.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
-import { AuthState } from "./auth_slice";
 import { FaceID } from "../face_capture";
 import axios from "axios";
 
@@ -13,45 +12,42 @@ import {
   // set_logout,
 } from "./auth_slice";
 
-const FaceLogin = (
-  data: AuthState,
-  dispatch: any,
-  setState: React.Dispatch<React.SetStateAction<boolean>>
-) => {
-  setState(true);
-  const url: string = "http://127.0.0.1:8000/api/login-face/";
-  const form_data = new FormData();
-  form_data.append("username", data.username);
-  form_data.append("img", data.image_base64);
-
-  const config = {
-    headers: { "content-type": "multipart/form-data" },
-  };
-
-  axios
-    .post(url, form_data, config)
-    .then((response) => {
-      if (response.data !== null) {
-        dispatch(set_logged());
-        dispatch(set_access_token(response.data["access"]));
-        dispatch(set_refresh_token(response.data["refresh"]));
-        setState(false);
-      }
-    })
-    .catch(() => {
-      setState(false);
-    });
-};
-
 const LoginFaceID = () => {
   const auth = useAppSelector((state) => state.auth);
   const dispatch = useAppDispatch();
 
   const [sending, setSending] = React.useState(false);
 
+  const FaceLogin = () => {
+    setSending(true);
+    const url: string = "http://127.0.0.1:8000/api/login-face/";
+    const form_data = new FormData();
+    form_data.append("username", auth.username);
+    form_data.append("img", auth.image_base64);
+
+    const config = {
+      headers: { "content-type": "multipart/form-data" },
+    };
+
+    axios
+      .post(url, form_data, config)
+      .then((response) => {
+        if (response.data !== null) {
+          dispatch(set_logged());
+          dispatch(set_access_token(response.data["access"]));
+          dispatch(set_refresh_token(response.data["refresh"]));
+          setSending(false);
+        }
+      })
+      .catch(() => {
+        setSending(false);
+      });
+  };
+
   React.useEffect(() => {
     document.title = "FaceID Login";
-  }, [auth.loggedIn]);
+  }, []);
+
   return auth.loggedIn ? (
     <div>Success</div>
   ) : (
@@ -67,10 +63,7 @@ const LoginFaceID = () => {
           />
         </label>
         <FaceID action={set_image_base64_log} />
-        <button
-          type="button"
-          onClick={() => FaceLogin(auth, dispatch, setSending)}
-        >
+        <button type="button" onClick={FaceLogin}>
           {sending ? "Please wait..." : "Login"}
         </button>
       </form>
--- frontend/frontend/src/features/auth/Login_Password.tsx
+++ frontend/frontend/src/features/auth/Login_Password.tsx
@@ -1,11 +1,75 @@
 import React from "react";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import axios from "axios";
 
+import {
+  set_logged,
+  set_username_log,
+  set_password_log,
+  set_access_token,
+  set_refresh_token,
+  // set_logout,
+} from "./auth_slice";
 const LoginPassword = () => {
+  const auth = useAppSelector((state) => state.auth);
+  const dispatch = useAppDispatch();
+
+  const [sending, setSending] = React.useState(false);
+
+  const PasswordLogin = () => {
+    setSending(true);
+    const url: string = "http://127.0.0.1:8000/api/login-password/";
+    const data = { username: auth.username, password: auth.password };
+
+    axios
+      .post(url, data)
+      .then((response) => {
+        if (response.data !== null) {
+          dispatch(set_logged());
+          dispatch(set_access_token(response.data["access"]));
+          dispatch(set_refresh_token(response.data["refresh"]));
+          setSending(false);
+        }
+      })
+      .catch(() => {
+        setSending(false);
+      });
+  };
+
   React.useEffect(() => {
     document.title = "Password Login";
   }, []);
 
-  return <div>Pass</div>;
+  return auth.loggedIn ? (
+    <div>Success</div>
+  ) : (
+    <div>
+      <form>
+        <label htmlFor="username">
+          Username:
+          <input
+            type="text"
+            id="username"
+            value={auth.username}
+            onChange={(e) => dispatch(set_username_log(e.currentTarget.value))}
+          />
+        </label>
+        <p></p>
+        <label htmlFor="password">
+          Password:
+          <input
+            type="password"
+            id="password"
+            value={auth.password}
+            onChange={(e) => dispatch(set_password_log(e.currentTarget.value))}
+          />
+        </label>
+        <button type="button" onClick={PasswordLogin}>
+          {sending ? "Please wait..." : "Login"}
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default LoginPassword;
